d719392a6ac644af90c51eb1158ace91ca0768d7
gregoryduckworth <gregoryduckworth@googlemail.com>
2019-01-30 13:59:02 +0000

    Improve mine layout

--- resources/views/mines.blade.php
+++ resources/views/mines.blade.php
@@ -5,8 +5,11 @@
 @section('content')
     <div class="col mx-auto mh-100" style="padding-top: 20px;">
         @foreach($mines as $key => $mine)
-            <li>{{ $mine->y }}, {{ $mine->x }} - {{ ucfirst($mine->mine->type) }}</li>
-            @if($key+1 != count($mines) && $mine->mine != $mines[$key+1]->mine)
+            @if(($key-1 != -1 && $mine->type != $mines[$key-1]->type) || $key == 0)
+              <h3>{{ ucfirst($mine->type) }}</h3>
+            @endif
+            <li>{{ $mine->y }}, {{ $mine->x }}</li>
+            @if($key + 1 != count($mines) && $mine->mine != $mines[$key + 1]->mine)
               <hr>
             @endif
         @endforeach
